9f6e7a5337dc0feedfa12968ed6edfa45c9beaee
Yuvan12128 <yuvan12128@gmail.com>
2025-01-21 17:33:55 +0530

    update my code

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import About from "./components/About"
 import Footer from "./components/Footer"
 import NavBar from "./components/NavBar"
+import Skills from "./components/Skills"
 
 function App() {
   
@@ -10,6 +11,7 @@ function App() {
      <NavBar/>
      <About/>
      <Footer/>
+     <Skills/>
     </>
   )
 }
---
+++ src/components/Skills.jsx
@@ -0,0 +1,187 @@
+import React from 'react'
+
+const Skills = () => {
+    return (
+        <>
+
+
+            <div class="flex items-center justify-center py-4 md:py-8 flex-wrap">
+                <div>
+                    <div class=" w-full max-w-sm p-4 bg-white border border-gray-200 m-2 shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700 ">
+                        <h5 class="mb-4 text-xl font-medium text-gray-500 dark:text-gray-400">Front-End Skills</h5>
+                        <div class="flex items-baseline text-gray-900 dark:text-white">
+                            <span class="text-3xl font-semibold"></span>
+                            <span class="text-5xl font-extrabold tracking-tight">
+                                <i class="fa-brands fa-html5"></i>  <i class="fa-brands fa-css3-alt"></i> <i class="fa-brands fa-js"></i> <i class="fa-brands fa-react"></i> <i class="fa-brands fa-bootstrap"></i>
+
+                            </span>
+                            <span class="ms-1 text-xl font-normal text-gray-500 dark:text-gray-400"></span>
+                        </div>
+                        <ul role="list" class="space-y-5 my-7">
+                            <li class="flex items-center">
+                                <svg class="flex-shrink-0 w-4 h-4 text-blue-700 dark:text-blue-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                                    <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+                                </svg>
+                                <span class="text-base font-normal leading-tight text-gray-500 dark:text-gray-400 ms-3">HTML</span>
+                            </li>
+                            <li class="flex">
+                                <svg class="flex-shrink-0 w-4 h-4 text-blue-700 dark:text-blue-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                                    <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+                                </svg>
+                                <span class="text-base font-normal leading-tight text-gray-500 dark:text-gray-400 ms-3">CSS</span>
+                            </li>
+                            <li class="flex">
+                                <svg class="flex-shrink-0 w-4 h-4 text-blue-700 dark:text-blue-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                                    <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+                                </svg>
+                                <span class="text-base font-normal leading-tight text-gray-500 dark:text-gray-400 ms-3">JavaScript</span>
+                            </li>
+                            <li class="flex">
+                                <svg class="flex-shrink-0 w-4 h-4 text-blue-700 dark:text-blue-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                                    <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+                                </svg>
+                                <span class="text-base font-normal leading-tight text-gray-500 dark:text-gray-400 ms-3">React Js</span>
+                            </li>
+                            <li class="flex">
+                                <svg class="flex-shrink-0 w-4 h-4 text-blue-700 dark:text-blue-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                                    <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+                                </svg>
+                                <span class="text-base font-normal leading-tight text-gray-500 dark:text-gray-400 ms-3">BootStrap</span>
+                            </li>
+                            <li class="flex">
+                                <svg class="flex-shrink-0 w-4 h-4 text-blue-700 dark:text-blue-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                                    <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+                                </svg>
+                                <span class="text-base font-normal leading-tight text-gray-500 dark:text-gray-400 ms-3">Tailwind CSS</span>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div>
+                    <div>
+                        <div class="w-full max-w-sm p-4 bg-white border border-gray-200 m-2 shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700 ">
+                            <h5 class="mb-4 text-xl font-medium text-gray-500 dark:text-gray-400">Back-End Skills</h5>
+                            <div class="flex items-baseline text-gray-900 dark:text-white">
+                                <span class="text-3xl font-semibold"></span>
+                                <span class="text-5xl font-extrabold tracking-tight">
+                                <i class="fa-brands fa-java"></i>
+
+                                </span>
+                                <span class="ms-1 text-xl font-normal text-gray-500 dark:text-gray-400"></span>
+                            </div>
+                            <ul role="list" class="space-y-5 my-7">
+                                <li class="flex items-center">
+                                    <svg class="flex-shrink-0 w-4 h-4 text-blue-700 dark:text-blue-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                                        <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+                                    </svg>
+                                    <span class="text-base font-normal leading-tight text-gray-500 dark:text-gray-400 ms-3">Java</span>
+                                </li>
+                                <li class="flex">
+                                    <svg class="flex-shrink-0 w-4 h-4 text-blue-700 dark:text-blue-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                                        <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+                                    </svg>
+                                    <span class="text-base font-normal leading-tight text-gray-500 dark:text-gray-400 ms-3">Spring Boot</span>
+                                </li>
+                                
+
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div>
+                        <div class="w-full max-w-sm p-4 bg-white border border-gray-200 m-2 shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700 ">
+                            <h5 class="mb-4 text-xl font-medium text-gray-500 dark:text-gray-400">Database Skills</h5>
+                            <div class="flex items-baseline text-gray-900 dark:text-white">
+                                <span class="text-3xl font-semibold"></span>
+                                <span class="text-5xl font-extrabold tracking-tight">
+                                <i class="fa-solid fa-database"></i>
+
+                                </span>
+                                <span class="ms-1 text-xl font-normal text-gray-500 dark:text-gray-400"></span>
+                            </div>
+                            <ul role="list" class="space-y-5 my-7">
+                                <li class="flex items-center">
+                                    <svg class="flex-shrink-0 w-4 h-4 text-blue-700 dark:text-blue-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                                        <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+                                    </svg>
+                                    <span class="text-base font-normal leading-tight text-gray-500 dark:text-gray-400 ms-3">MySQL</span>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div>
+                        <div class="w-full max-w-sm p-4 bg-white border border-gray-200 m-2 shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700 ">
+                            <h5 class="mb-4 text-xl font-medium text-gray-500 dark:text-gray-400">Version Control</h5>
+                            <div class="flex items-baseline text-gray-900 dark:text-white">
+                                <span class="text-3xl font-semibold"></span>
+                                <span class="text-5xl font-extrabold tracking-tight">
+                                <i class="fa-brands fa-github"></i>
+                                </span>
+                                <span class="ms-1 text-xl font-normal text-gray-500 dark:text-gray-400"></span>
+                            </div>
+                            <ul role="list" class="space-y-5 my-7">
+                                <li class="flex items-center">
+                                    <svg class="flex-shrink-0 w-4 h-4 text-blue-700 dark:text-blue-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                                        <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+                                    </svg>
+                                    <span class="text-base font-normal leading-tight text-gray-500 dark:text-gray-400 ms-3">Git </span>
+                                </li>
+                                <li class="flex items-center">
+                                    <svg class="flex-shrink-0 w-4 h-4 text-blue-700 dark:text-blue-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                                        <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+                                    </svg>
+                                    <span class="text-base font-normal leading-tight text-gray-500 dark:text-gray-400 ms-3">GitHub</span>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div>
+                        <div class=" w-full max-w-sm p-4 bg-white border border-gray-200 m-2 shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700 ">
+                            <h5 class="mb-4 text-xl font-medium text-gray-500 dark:text-gray-400">IDEs & Tools</h5>
+                            <div class="flex items-baseline text-gray-900 dark:text-white">
+                                <span class="text-3xl font-semibold"></span>
+                                <span class="text-5xl font-extrabold tracking-tight">
+                                <i class="fa-brands fa-windows"></i> <i class="fa-brands fa-linux"></i> <i class="fa-brands fa-ubuntu"></i>
+                                </span>
+                                <span class="ms-1 text-xl font-normal text-gray-500 dark:text-gray-400"></span>
+                            </div>
+                            <ul role="list" class="space-y-5 my-7">
+                                <li class="flex items-center">
+                                    <svg class="flex-shrink-0 w-4 h-4 text-blue-700 dark:text-blue-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                                        <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+                                    </svg>
+                                    <span class="text-base font-normal leading-tight text-gray-500 dark:text-gray-400 ms-3">Windows </span>
+                                </li>
+                                <li class="flex items-center">
+                                    <svg class="flex-shrink-0 w-4 h-4 text-blue-700 dark:text-blue-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                                        <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+                                    </svg>
+                                    <span class="text-base font-normal leading-tight text-gray-500 dark:text-gray-400 ms-3">Linux Ubuntu</span>
+                                </li>
+                                <li class="flex items-center">
+                                    <svg class="flex-shrink-0 w-4 h-4 text-blue-700 dark:text-blue-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                                        <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+                                    </svg>
+                                    <span class="text-base font-normal leading-tight text-gray-500 dark:text-gray-400 ms-3">Linux Mint</span>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+
+
+
+
+        </>
+    )
+}
+
+export default Skills
